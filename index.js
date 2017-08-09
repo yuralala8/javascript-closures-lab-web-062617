e7f37e814416e018db2bfb0cd70189bf11dbdab8
Yura Choi <yc1435@nyu.edu>
2017-08-09 13:58:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+    let counter = 0;
+  function addBump() {
+    counter++
+  }
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+
+  };
+}
+
+
+function createAnimal(animalType) {
+  return function createDevice(deadlyDevice){
+  let obj = {}
+    obj["animalType"] = animalType
+    obj["deadlyDevice"] = deadlyDevice
+  return obj
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = createAnimal('Shark')('Laserbeam')
+var sharkWithFrickinCannon = createAnimal('Shark')('Cannon')
